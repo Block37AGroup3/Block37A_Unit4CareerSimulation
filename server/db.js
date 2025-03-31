5710e858a590eb098944ab7c2a89ca0f051cbc83
Andy Edwards <andyedwards@outlook.com>
2025-03-31 13:58:05 -0400

    created drop table

--- server/db.js
+++ server/db.js
@@ -1,20 +1,37 @@
 require("dotenv").config();
 
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost:5432/block37_db');
+const pg = require("pg");
+const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost:5432/block37_db");
+
+//create tables
+const createTables = async () => {
+  // Hopefully the right order to prevent foreign key issue
+  const SQL = /*sql*/ `
+  DROP TABLE IF EXISTS Comments;
+  DROP TABLE IF EXISTS Reviews;
+  DROP TABLE IF EXISTS Items;
+  DROP TABLE IF EXISTS Users;
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+
+      );
+      `;
+};
 
 const connectDB = async () => {
   try {
-    console.log('Connecting to database...');
+    console.log("Connecting to database...");
     await client.connect();
-    console.log('Connected to database.');
+    console.log("Connected to database.");
   } catch (error) {
-    console.error('Database connection error:', error);
+    console.error("Database connection error:", error);
     process.exit(1);
   }
 };
 
 module.exports = {
   client,
-  connectDB
-}
+  connectDB,
+};

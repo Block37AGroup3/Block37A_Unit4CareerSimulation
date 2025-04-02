477c32bb98b63a0309d018a1dd4fee67f80b09c7
apelic <apelic92@gmail.com>
2025-04-02 16:34:45 -0500

    rm pseudocode

--- server/index.js
+++ server/index.js
@@ -166,7 +166,6 @@ app.post("/api/items/:itemId/reviews", isLoggedIn, async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
 //GET /api/reviews/me route
 
 app.get('/api/reviews/me', isLoggedIn, async (req, res) => {

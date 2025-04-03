019377481c95f83885b212596abb1a72b51f0afc
Andy Edwards <andyedwards@outlook.com>
2025-04-03 10:57:12 -0400

    had to add bcrypt and jwt to top

--- server/index.js
+++ server/index.js
@@ -23,6 +23,8 @@ const {
 const { seedData } = require("./seed.js");
 
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3000;
 app.use(express.json());
